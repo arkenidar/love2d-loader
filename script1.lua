0fa9eb72f4fa5f3fa2a72c56a03fcf7a1682a7ec
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-05-05 15:20:20 +0200

    some Lua comments were removed for clean-up

--- script1.lua
+++ script1.lua
@@ -1,25 +1,6 @@
 function love.draw()
-  common_draw()
+    common_draw()
     love.graphics.print('Hello World from script1!', 100, 100)
 end
-
---[[
-function love.update()
-	if love.mouse.isDown(1) then
-    love.filesystem.load("script2.lua")()
-	end
-end
---]]
---[[
-function love.mousereleased(x, y, button)
-   if button == 1 then
-      love.filesystem.load("script2.lua")()
-   end
-end
---]]
-
 next="script2.lua"
--------------
-
----dofile("location.lua")
 love.filesystem.load("location.lua")()
